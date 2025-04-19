c2f751ea31caab9bcf58b1ebe07280684ef26660
me <me@me.dev>
2025-04-18 17:17:34 -0700

    Wording has been edited

--- src/view/display/warning.ts
+++ src/view/display/warning.ts
@@ -3,6 +3,6 @@ import { text } from 'hyperapp'
 
 export const warning = () => {
   return h('div', {class: ''}, [
-    h('p', {}, text("open this tab on pump.fun's coin or swap page to see if dex is paid"))
+    h('p', {}, text("open this tool on pump.fun's coin or swap page to see if dex is paid"))
   ])
 }
